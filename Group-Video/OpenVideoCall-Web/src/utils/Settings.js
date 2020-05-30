b0143f22ccb8951582bbd1911f7fe1648c95bece
Oliver Canosa <50088503+OliverC4@users.noreply.github.com>
2020-05-30 01:12:50 -0400

    Update Settings.js

--- Group-Video/OpenVideoCall-Web/src/utils/Settings.js
+++ Group-Video/OpenVideoCall-Web/src/utils/Settings.js
@@ -7,7 +7,7 @@ export const RESOLUTION_ARR = {
 
 export const SHARE_ID = 1;
 
-export const APP_ID = "<#YOUR APP ID HERE#>";
+export const APP_ID = "289589f6db144a76a59e914e71ad26d0";
 
 // Assign Token to null if you have not enabled app certificate
-export const Token = "<#YOUR TEMP TOKEN HERE#>";
+export const Token = "006289589f6db144a76a59e914e71ad26d0IABdB77lJ3NS+ZMVoj3Gc1FaBczsVx1Dc6cgVxy0Q5AybRFWz+8AAAAAEADxldpwtjvTXgEAAQC0O9Ne";
